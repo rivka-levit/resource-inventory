c5f5899d72db6182651513f6380c6b28cf3a4b18
rivka-yuliia <juliacreadora@gmail.com>
2024-03-24 11:20:43 +0200

    total property

--- resource.py
+++ resource.py
@@ -1,9 +1,20 @@
 class Resource:
     """Resource object"""
 
-    def __init__(self, name, manufacturer=None):
+    def __init__(self, name, manufacturer=None, total=1):
         self.name = name
         self.manufacturer = manufacturer
+        self.total = total
+
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, value):
+        if value <= 0:
+            raise ValueError('Total must be greater than 0!')
+        self._total = value
 
     def __str__(self):
         return f'Resource: {self.name}'
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -20,3 +20,25 @@ class TestResourceInit(TestCase):
         resource = Resource('GeForce Experience', manufacturer=mf)
 
         self.assertEqual(resource.manufacturer, mf)
+
+    def test_create_resource_with_total_success(self):
+        """Test creating a resource instance with total successfully."""
+
+        total = 5
+        resource = Resource('Intel Core i9-9900K', total=total)
+
+        self.assertEqual(resource.total, total)
+
+    def test_create_resource_with_neg_total_fails(self):
+        """Test creating a resource instance with negative total fails."""
+
+        total = -5
+        with self.assertRaises(ValueError):
+            Resource('Intel Core i9-9900K', total=total)
+
+    def test_create_resource_with_zero_total_fails(self):
+        """Test creating a resource instance with zero total fails."""
+
+        total = 0
+        with self.assertRaises(ValueError):
+            Resource('Intel Core i9-9900K', total=total)
