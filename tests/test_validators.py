36352e32f8acba218e125ff2dc357b1864726556
rivka-yuliia <juliacreadora@gmail.com>
2024-04-03 16:31:04 +0300

    validator tests with coverage

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -24,20 +24,49 @@ class TestIntegerValidator(TestCase):
         with self.assertRaises(TypeError):
             validate_integer('arg', 1.5, 0, 20)
 
-    def test_invalid_min(self):
+    def test_min_std_err_msg(self):
         """
         Test validation raises an error when the argument is less than
         minimum value.
         """
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as e:
             validate_integer('arg', -5, 0, 20)
 
-    def test_invalid_max(self):
+        self.assertIn('arg', e.exception.args[0])
+        self.assertIn('0', e.exception.args[0])
+
+    def test_min_custom_err_msg(self):
+        """
+        Test validation raises an error when the argument is less than
+        minimum value, and gives a custom error message.
+        """
+
+        with self.assertRaises(ValueError) as e:
+            validate_integer('arg', -5, 0, 20, 'custom min msg')
+
+        self.assertEqual('custom min msg', e.exception.args[0])
+
+    def test_max_std_err_msg(self):
         """
         Test validation raises an error when the argument is greater than
         maximum value.
         """
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as e:
             validate_integer('arg', 25, 0, 20)
+
+        self.assertIn('arg', e.exception.args[0])
+        self.assertIn('20', e.exception.args[0])
+
+    def test_max_custom_err_msg(self):
+        """
+        Test validation raises an error when the argument is greater than
+        maximum value, and gives a custom error message.
+        """
+
+        with self.assertRaises(ValueError) as e:
+            validate_integer('arg', 25, 0, 20,
+                             'custom min msg', 'custom max msg')
+
+        self.assertEqual('custom max msg', e.exception.args[0])
