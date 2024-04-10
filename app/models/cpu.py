115cc7091f62935267269c32de9dd3dbef7c256a
rivka-yuliia <juliacreadora@gmail.com>
2024-04-10 18:00:11 +0300

    change cpu and tests

--- app/models/cpu.py
+++ app/models/cpu.py
@@ -1,28 +1,59 @@
 from app.models.resource import Resource
 
+from app.utils.validators import validate_integer
+
 
 class CPU(Resource):
-    """CPU resource"""
+    """Resource subclass used to track specific CPU inventory pools."""
 
-    def __init__(self, name, cores, socket, power_watts, *,
-                 manufacturer=None, total=1):
-        super().__init__(name, manufacturer=manufacturer, total=total)
+    def __init__(self, name, total, allocated, cores, socket, power_watts, *,
+                 manufacturer=None):
+        """
+        Args:
+            name (str): display name of the resource
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            cores (int): number of cores
+            socket (str): CPU socket type
+            power_watts (int): CPU rated wattage
+            manufacturer (str): resource manufacturer
+        """
+
+        super().__init__(name, total, allocated, manufacturer=manufacturer)
+
+        validate_integer('cores', cores, min_value=1)
+        validate_integer('power_watts', power_watts, min_value=1)
+
         self._cores = cores
         self._socket = socket
         self._power_watts = power_watts
 
     @property
     def cores(self):
+        """
+        Number of cores.
+        Returns: int
+        """
+
         return self._cores
 
     @property
     def socket(self):
+        """
+        The socket type for CPU.
+        Returns: str
+        """
+
         return self._socket
 
     @property
     def power_watts(self):
+        """
+        The rated wattage for CPU.
+        Returns: int
+        """
+
         return self._power_watts
 
     def __repr__(self):
-        return (f'CPU(name={self.name}, cores={self.cores}, '
-                f'socket={self.socket}, power_watts={self.power_watts})')
+        return f'{self.category}: {self.name} ({self.socket} - x{self.cores})'
--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -1,3 +1,8 @@
+"""
+Tests for CPU class.
+Command line: python -m unittest -v tests.test_cpu
+"""
+
 from unittest import TestCase
 from app.models.cpu import CPU
 
@@ -5,29 +10,32 @@ from app.models.cpu import CPU
 class TestCPU(TestCase):
     """Tests for CPU class."""
 
+    def setUp(self):
+        self.cpu_values = {
+            'name': 'RYZEN Threadripper 2990WX',
+            'total': 10,
+            'allocated': 3,
+            'cores': 32,
+            'socket': 'sTR4',
+            'power_watts': 250,
+            'manufacturer': 'AMD',
+        }
+
     def test_create_cpu_resource(self):
+        """Test creating CPU resource."""
 
-        name = '5 x AMD Ryzen'
-        cores = 2
-        socket = 'AM4'
-        power = '94'
+        cpu = CPU(**self.cpu_values)
 
-        cpu = CPU(name=name, cores=cores, socket=socket, power_watts=power)
-
-        self.assertEqual(cpu.cores, cores)
-        self.assertEqual(cpu.socket, socket)
-        self.assertEqual(cpu.power_watts, power)
+        for key, value in self.cpu_values.items():
+            self.assertEqual(getattr(cpu, key), value)
 
     def test_cpu_representation(self):
         """Test CPU representation."""
 
-        name = '5 x AMD Ryzen'
-        cores = 2
-        socket = 'AM4'
-        power = '94'
-        expected_repr = (f'CPU(name={name}, cores={cores}, socket={socket}, '
-                         f'power_watts={power})')
+        expected_repr = (f'cpu: {self.cpu_values["name"]} '
+                         f'({self.cpu_values["socket"]} - '
+                         f'x{self.cpu_values["cores"]})')
 
-        cpu = CPU(name=name, cores=cores, socket=socket, power_watts=power)
+        cpu = CPU(**self.cpu_values)
 
         self.assertEqual(repr(cpu), expected_repr)
